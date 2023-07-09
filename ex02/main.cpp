072d63b395a1655422b03e5c1e51232f65432fe7
JLeson <joelburleson0104@gmail.com>
2023-07-09 12:49:26 +0200

    ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joel <joel@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/09 12:43:32 by joel              #+#    #+#             */
+/*   Updated: 2023/07/09 12:47:45 by joel             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	std::string	str = "HI THIS IS BRAIN";
+
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
+
+	std::cout << &str << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+	std::cout << std::endl;
+	std::cout << str << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+	return (0);
+}
